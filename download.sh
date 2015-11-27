8a486768103d776b15d6fb9e06b8552e864c6dc6
Joe Wass <joe@afandian.com>
2015-11-27 11:52:06 +0000

    Tidy up directory structure. Add latest symlink.

--- download.sh
+++ download.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 datestamp=$(date +%y-%m-%d-%H:%M:%S)
-dirname="run/$datestamp"
-mkdir -p $dirname
-cp urls.txt $dirname/urls.txt
-cd $dirname
-echo "Downloading into $dirname"
-wget --verbose --header="CR-TDM-Client-Token: $TDM_TOKEN" -i urls.txt
+outputdir="`pwd`/run/$datestamp"
+latestdir="`pwd`/latest"
+mkdir -p $outputdir
+rm $latestdir
+ln -s $outputdir $latestdir
+cp urls.txt $outputdir/urls.txt
+cd $outputdir
+echo "Downloading into $outputdir"
+wget --verbose --header="CR-TDM-Client-Token: $TDM_TOKEN" --header="CR-Clickthrough-Client-Token: $TDM_TOKEN" -i urls.txt
+
